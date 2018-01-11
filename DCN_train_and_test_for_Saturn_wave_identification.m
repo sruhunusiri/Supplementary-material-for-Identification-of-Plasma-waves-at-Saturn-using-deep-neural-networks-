b3c610c92e0312242cc42c23d8d004afb56b1d6a
sruhunusiri <34774464+sruhunusiri@users.noreply.github.com>
2018-01-11 16:29:54 -0600

    Update DCN_train_and_test_for_Saturn_wave_identification.m

--- DCN_train_and_test_for_Saturn_wave_identification.m
+++ DCN_train_and_test_for_Saturn_wave_identification.m
@@ -1,6 +1,5 @@
 % Supplementary material to Suranga Ruhunusiri, "Identification of Plasma waves at Saturn 
-% using deep neural networks", to be published in IEEE Transactions on Geoscience and 
-% Remote Sensing, 2018.
+% using deep neural networks", to be published in IEEE Transactions on Plasma Science, 2018.
 
 % This program has been tested with MATLAB R2017a using a NVDIA GeForce GTX 750 Ti GPU on Windows 10.
 
